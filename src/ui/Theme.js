ad2dde69a9a19c3d34f1bf1b4867da406c8d8652
Jess <jessbonanno@gmail.com>
2020-07-22 19:42:21 -0400

    setup theme skeleton

---
+++ src/ui/Theme.js
@@ -0,0 +1,59 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+/**
+ * initializes colors and font settings for app theme
+ *
+ *
+ * @returns {default theme css}
+ * @constructor
+ */
+
+// * fonts
+
+// * colors
+
+
+export default createMuiTheme({
+  palette: {
+    common: {},
+    primary: {},
+    secondary: {},
+
+    action: {
+      //       active:
+      // rgba(0, 0, 0, 0.54)
+      // hover:
+      // rgba(0, 0, 0, 0.04)
+      // hoverOpacity: 0.04
+      // selected:
+      // rgba(0, 0, 0, 0.08)
+      // selectedOpacity: 0.08
+      // disabled:
+      // rgba(0, 0, 0, 0.26)
+      // disabledBackground:
+      // rgba(0, 0, 0, 0.12)
+      // disabledOpacity: 0.38
+      // focus:
+      // rgba(0, 0, 0, 0.12)
+      // focusOpacity: 0.12
+      // activatedOpacity: 0.12
+    },
+    primary: {},
+    secondary: {},
+  },
+  typography: {
+    h1: {},
+    h2: {},
+    h3: {},
+    h4: {},
+    h5: {},
+    h6: {},
+    p: {},
+    subtitle1: {},
+    subtitle2: {},
+    body1: {},
+    body2: {},
+    button: {},
+    caption: {},
+  },
+});
