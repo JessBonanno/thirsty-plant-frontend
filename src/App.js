16c67534d79c93de3e46d05defec15f83fe4e338
Jess <jessbonanno@gmail.com>
2020-07-22 22:08:36 -0400

    created delete modal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
 import theme from './components/ui/Theme';
 
+// * local imports
+import DeleteModal from './components/DeleteModal';
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <h1>water my plants!</h1>
+      {/* DeleteModal component here for dev only will be moved */}
+      <DeleteModal />
     </ThemeProvider>
   );
 }
