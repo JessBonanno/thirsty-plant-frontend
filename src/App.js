8997a0e106c79f0fe412b6937144b2fce66454b0
Ben Venker <bvemails@gmai.com>
2020-07-28 09:19:37 -0500

    add private routes to app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Login from './components/Log-in.js';
 import Signup from './components/Sign-up.js';
 import AddPlant from './components/AddPlant.js';
 import Dashboard from './components/containers/Dashboard';
-import ChangePass from './components/ChangePass.js';
+import EditUser from './components/EditUser.js';
 import AppBar from './components/ui/AppBar';
 import Footer from './components/ui/Footer';
 import PrivateRoute from './utils/PrivateRoute';
@@ -27,9 +27,8 @@ function App() {
         <div className="App">
           <Route path="/login" component={Login} />
           <Route path="/Signup" component={Signup} />
-          <Route path="/ChangePass" component={ChangePass} />
-          <Route path="/AddPlant" component={AddPlant} />
-          <Route path="/dashboard" component={Dashboard} />
+          <PrivateRoute path="/settings" component={EditUser} />
+          <PrivateRoute path="/dashboard" component={Dashboard} />
         </div>
       </flexbox>
       <Hidden mdUp>
