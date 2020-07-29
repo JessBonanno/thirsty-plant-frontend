1836890fe627ae24f4428998b01dab70526db1c7
Ben Venker <bvemails@gmai.com>
2020-07-29 09:41:17 -0500

    make search bar take full-ish width on mobile

--- src/components/containers/Dashboard.js
+++ src/components/containers/Dashboard.js
@@ -49,7 +49,7 @@ const useStyles = makeStyles(theme => ({
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     transition: theme.transitions.create('width'),
-    width: '100%',
+    width: 250,
     [theme.breakpoints.up('sm')]: {
       width: '12ch',
       '&:focus': {
