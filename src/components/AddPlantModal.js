5d78277c1c0ed3804d2a1795218e085052465f69
Ben Venker <bvemails@gmai.com>
2020-07-29 09:41:03 -0500

    constrain input to 15 characters

--- src/components/AddPlantModal.js
+++ src/components/AddPlantModal.js
@@ -148,6 +148,9 @@ export default function TransitionsModal(props) {
                     <form>
                       <Grid item>
                         <TextField
+                          inputProps={{
+                            maxLength: 15,
+                          }}
                           className={classes.formField}
                           variant="outlined"
                           label="Plant name"
