d046a6590269de44aafb7a4b615b54aa1294fd8e
Jess <jessbonanno@gmail.com>
2020-07-31 15:15:05 -0400

    removed unused context

--- src/components/DeleteDialog.js
+++ src/components/DeleteDialog.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -6,9 +6,6 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
-// context
-import { PlantContext } from '../contexts/PlantContext';
-
 // api imports
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
